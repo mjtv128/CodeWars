fe7bd7e58472b0955f448d6aa722ec5c5cdeea25
Michelle Van <53450671+mjtv128@users.noreply.github.com>
2020-01-17 15:38:17 +0000

    Add files via upload

---
+++ 7kyu-sort_array_by_string_length/test_code.py
@@ -0,0 +1,29 @@
+
+import unittest
+import code
+
+class TestCode(unittest.TestCase):
+    def test_one(self):
+        test = ["beg", "life", "i", "to"]
+        self.assertEqual(code.sort_by_length(test), ["i", "to", "beg", "life"])
+
+    def test_two(self):
+        test = ["", "moderately", "brains", "pizza"]
+        self.assertEqual(code.sort_by_length(test), ["", "pizza", "brains", "moderately"])
+
+if __name__ == '__main__':
+    unittest.main()
+
+# test.describe("Example Tests")
+
+# tests = [
+#     [["i", "to", "beg", "life"], ["beg", "life", "i", "to"]],
+#     [["", "pizza", "brains", "moderately"], ["", "moderately", "brains", "pizza"]],
+#     [["short", "longer", "longest"], ["longer", "longest", "short"]],
+#     [["a", "of", "dog", "food"], ["dog", "food", "a", "of"]],
+#     [["", "bees", "eloquent", "dictionary"], ["", "dictionary", "eloquent", "bees"]],
+#     [["a short sentence", "a longer sentence", "the longest sentence"], ["a longer sentence", "the longest sentence", "a short sentence"]],
+# ]
+
+# for exp, inp in tests:
+#     test.assert_equals(sort_by_length(inp), exp)
